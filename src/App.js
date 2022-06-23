de40174f1fcd4e1db35d403c1618e1cbb4e8a06c
Arif Puji Setiawan <arif.p.setiawan.9@gmail.com>
2022-06-23 21:26:51 +0700

    Fix: Styling App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import "./App.css"
 
-import React from 'react';
+import React from "react"
 
-import ToDo from './containers/ToDo';
+import ToDo from "./containers/ToDo"
+
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
 
 const App = () => {
   return (
     <div className="App">
       <ToDo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
